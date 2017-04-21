9c1c163748c21c55ac0c0e26dcddd24bd9824629
Hong Hao <oahong@gmail.com>
2017-04-21 12:54:09 +0800

    Run debian-cd script with env to build deepin cd

--- deepin_cd/deepin_cd.py
+++ deepin_cd/deepin_cd.py
@@ -23,60 +23,46 @@ class DebianCD(object):
     debian_cd_url = 'http://git.sh.sndu.cn/hhao/debian-cd'
     id_file = 'CURRENT_BUILD_ID'
 
-    def __init__(self, arch, version, build_id, workdir, name='deepin'):
+    def __init__(self, arch, version, build_id, work, output, name='deepin'):
         """
         name is a custom cd name, in most cases it's deepin
         """
         self.arch = arch
-        self.version = version
-        self.build_id = build_id
+        self.version = str(version)
+        self.build_id = str(build_id)
         self.name = name
-        self.workdir = workdir
+        self.work = work
+        self.output = output
+        self.mirror = os.path.join(
+            os.path.dirname(self.work), 'mirrors', self.arch)
 
         self.target_cd = '{}-{}-{}-B{}-DVD-1.iso'.format(
             self.name, version, arch, build_id)
-        logger.info('Target media is {}'.format(self.target_cd))
+        logger.info('Target media is {} lives in {}'.format(self.target_cd, self.output))
 
     def get_output_dir(self):
-        return os.path.join(self.workdir, 'output', self.version)
+        return os.path.join(self.output, self.version, str(date.today()))
 
     def get_build_id(self):
-        with open(os.path.join(
-                self.get_output_dir(), self.id_file), 'r') as f:
-            build_id = f.readline().strip()
-
-        if build_id.isdigit():
-            return int(build_id)+1
-        else:
-            return 1
-
-    def set_build_id(self, id):
-        if type(id) == int:
-            id += 1
-        else:
-            raise ValueError("id: {} is invalid".format(id))
-        with open(os.path.join(
-                self.get_output_dir(), self.id_file), 'w') as f:
-            f.write('\n'.join(id))
+        return self.build_id
 
     def get_artifact(self):
         """
         validate the final image
         """
         artifact = os.path.join(self.get_output_dir(),
-                                str(date.today()),
                                 self.target_cd)
         if os.path.exists(artifact):
             return artifact
         else:
             raise IOError('{} not found'.format(artifact))
 
-    def initialize_workdir(self):
-        os.makedirs(self.workdir, exist_ok=True)
-        if not os.listdir(self.workdir):
+    def initialize_work(self):
+        os.makedirs(self.work, exist_ok=True)
+        if not os.listdir(self.work):
             logger.info('fetching debian-cd source code into {}'.format(
-                self.workdir))
-            self.runcmd(['git', 'clone', DebianCD.debian_cd_url, self.workdir])
+                self.work))
+            self.runcmd(['git', 'clone', DebianCD.debian_cd_url, self.work])
 
     @staticmethod
     def runcmd(cmd, env={}):
@@ -84,11 +70,10 @@ class DebianCD(object):
         Run cmd with env, check return code then print stdout
         """
         logger.debug('runcmd %s with env %s', cmd, env)
-        if env:
-            cp = run(cmd, stdout=PIPE, check=True, env=env)
-        else:
-            cp = run(cmd, stdout=PIPE, check=True)
+        cp = run(cmd, stdout=PIPE, env=env)
         print(cp.stdout.decode())
+        if not cp.returncode:
+            raise subprocess.CalledProcessError('Failed to run', cmd)
 
     @staticmethod
     def append_package_list(package_list, taskfile):
@@ -118,7 +103,6 @@ class DebianCD(object):
         """
         A debian-cd wrapper.
         """
-        os.chdir(self.workdir)
         logger.info("Start to build ISO image")
         self.runcmd(['bash', 'easy-build.sh', '-d', 'light', 'BC', self.arch])
         self.get_artifact()
@@ -143,9 +127,16 @@ class DeepinCD(DebianCD):
         - write artifacts to output dir, which is set via configuration or command line parameter
         """
         logger.info("Start to build ISO image for %s", self.arch)
-        os.chdir(os.path.join(self.workdir, 'deepin', self.codename))
-        self.runcmd(['bash', '/tmp/build-cd1.sh'], env={'TEST': 'ABC'})
-
+        os.chdir(os.path.join(self.work))
+        self.runcmd(['bash', 'deepin-build.sh', 'DVD', self.arch],
+                    env={'PROJECT': self.name,
+                         'CDVERSION': self.version,
+                         'WORK': self.work,
+                         'OUTPUT': self.output,
+                         'BUILD_ID': self.build_id,
+                         'DEEPIN_MIRROR': self.mirror
+                    }
+        )
 
 if __name__ == '__main__':
     cd = DeepinCD('mips64el', '2014.3', 23, '/tmp')
