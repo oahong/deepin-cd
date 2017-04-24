932067fcc0d1f216b8adf33ae733439cc7f480f6
Hong Hao <oahong@gmail.com>
2017-04-24 22:12:25 +0800

    Define main function

--- __main__.py
+++ __main__.py
@@ -10,7 +10,7 @@ from deepin_cd.deepin_cd import DeepinCD
 from deepin_cd.config import __VERSION__, __ARCHS__
 from deepin_cd.utils import set_value
 
-if __name__ == '__main__':
+def parse_opts():
     #TODO: preseed not implemented
     parser = argparse.ArgumentParser(
         description='A custom installation media generator for deepin server.')
@@ -34,32 +34,32 @@ if __name__ == '__main__':
                         default='info', help='Set logging level')
     parser.add_argument('--version', '-v', action='version',
                         version='%(prog)s ' + __VERSION__)
-    options = parser.parse_args()
 
-    logging.basicConfig(level=getattr(logging, options.log_level.upper()))
-    logger = logging.getLogger(__name__)
-    logger.info('program started: %s, version: %s' % (parser.prog, __VERSION__))
+    return parser.parse_args()
 
+def main(options):
     """
-    make a deepin cd in 5 steps:
+    Make a deepin cd in 5 steps:
     1. Add arch (mips64el/sw64) specific boot files (hard-coded in debian-cd at the time of writing)
     2. Append a list of packages to deepin-extra task file
     3. Append a list of packages to exclude, which will be exclude from debian-cd explicitly
-    4. execute easy-build.sh, options can be specified by using environment variables
+    4. execute easy-build.sh, opts can be specified by using environment variables
     5. Print the final build artifacts
     """
-    work = options.work
-    task_dir = os.path.join(options.work, 'tasks', DeepinCD.codename)
+    logging.basicConfig(level=getattr(logging, opts.log_level.upper()))
+    logger = logging.getLogger(__name__)
+    logger.info('program started version: %s' % __VERSION__)
 
-    configs = {}
+    work = opts.work
+    task_dir = os.path.join(opts.work, 'tasks', DeepinCD.codename)
 
-    if options.config:
-        # options has been specified via json config
-        logger.info("load configuration from %s", options.config)
-        with open(options.config, 'r') as f:
-            configs = json.load(f)
-        config_dir = os.path.realpath(os.path.dirname(options.config))
-        logger.debug("dump configurations in %s:\n %s".format(
+    if opts.config:
+        # opts has been specified via json config
+        logger.info("load configuration from %s", opts.config)
+        with open(opts.config, 'r') as f:            configs = json.load(f)
+
+        config_dir = os.path.realpath(os.path.dirname(opts.config))
+        logger.debug("dump configurations in {}:\n {}".format(
             config_dir, pprint.pformat(configs, 4)))
     else:
         configs = {
@@ -67,17 +67,17 @@ if __name__ == '__main__':
             'name': '', 'preseed': '', 'task': ''
         }
 
-    arch = set_value(configs['arch'], options.arch, allow_empty=False)
-    build_id = set_value(configs['task'], options.build_id, allow_empty=False)
+    arch = set_value(configs['arch'], opts.arch, allow_empty=False)
+    build_id = set_value(configs['task'], opts.build_id, allow_empty=False)
 
     # fall back to deepin-server-15.1
     name = set_value(configs['name'], 'deepin-server')
     version = set_value(configs['tag'], '15.1')
 
-    exclude = set_value(configs['exclude'], options.exclude)
-    include = set_value(configs['include'], options.include)
-    preseed = set_value(configs['preseed'], options.preseed)
-    output  = set_value(configs['output'], options.output)
+    exclude = set_value(configs['exclude'], opts.exclude)
+    include = set_value(configs['include'], opts.include)
+    preseed = set_value(configs['preseed'], opts.preseed)
+    output  = set_value(configs['output'], opts.output)
 
     cd = DeepinCD(arch, version, build_id, work, output)
     cd.initialize_work()
@@ -90,3 +90,7 @@ if __name__ == '__main__':
 
     cd.make_disc()
     logger.info("programme finished")
+
+if __name__ == '__main__':
+    opts = parse_opts()
+    main(opts)
