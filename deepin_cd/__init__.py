9a6535f757461b04df09d806eb5904090bac2bee
Hong Hao <oahong@gmail.com>
2018-01-14 18:16:44 +0800

    Export DeepinCD

--- deepin_cd/__init__.py
+++ deepin_cd/__init__.py
@@ -3,9 +3,6 @@
 import sys
 import logging
 
-__all__ = ["deepin_cd"]
+__all__ = ["DeepinCD"]
 
 logger = logging.getLogger(__name__)
-
-if sys.version_info[:2] < (3, 5):
-    raise Exception('Python version is less than 3.5')
