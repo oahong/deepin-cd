7988a285683304087fa798119e35fa9276c56504
Hong Hao <oahong@gmail.com>
2018-01-24 13:49:53 +0800

    s/sw64/sw_64/

--- deepin_cd/config.py
+++ deepin_cd/config.py
@@ -5,7 +5,7 @@ __email__ = 'hhao@oahong.me'
 __license__ = 'GPL-2'
 __version__ = '0.5.0'
 
-# supported architectures
-__architectures__ = ('mips64el', 'sw64')
 # deepin 15.x codename is kui
 __codenames__ = ('kui')
+# supported architectures: sw_64 should be sw64
+__architectures__ = ('mips64el', 'sw_64')
